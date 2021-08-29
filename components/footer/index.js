aba2655e70fc9657fb939fa99eb6c708d08ffff1
Sergii <sergiicherpak1@gmail.com>
2021-08-29 05:25:48 +0300

    update carousel

--- components/footer/index.js
+++ components/footer/index.js
@@ -46,7 +46,7 @@ const Footer = () => {
       
       <div className="site-footer__bottom">
         <div className="container">
-          <p>DESIGN BY ICEO.CO - © 2019. ALL RIGHTS RESERVED.</p>
+          <p>DESIGN BY SERGII CHERPAK - © 2019. ALL RIGHTS RESERVED.</p>
         </div>
       </div>
     </footer>
